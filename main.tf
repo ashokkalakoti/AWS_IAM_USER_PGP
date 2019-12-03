fd0404664fd9a55931d9a02ff5285c03875ae42d
Ashok Kalakoti <kashu9999@gmail.com>
2019-12-03 21:03:02 +0800

    adding decrypted password file

--- main.tf
+++ main.tf
@@ -10,3 +10,10 @@ resource "aws_iam_user_login_profile" "aws_profile" {
   pgp_key = filebase64("./mypublickey.pub")
 }
 
+
+resource "null_resource" "shell" {
+
+  provisioner "local-exec" {
+    command = "echo ${aws_iam_user_login_profile.aws_profile.encrypted_password} | base64 --decode -d -i | keybase pgp decrypt >> ./decrypted_password.txt"
+  }
+}
